365e73d11f3800e4247463e0bb64cc4748982604
Renato Rosa Franco <Renato_ny@live.com>
2019-03-14 20:14:56 -0300

    Change constructor to componentDidMount on PersonList.js

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,14 +7,12 @@ class PersonList extends Component {
 		persons: []
 	}
 
-	constructor() {
-		super();
-		
+	componentDidMount() {
 		axios.get('https://jsonplaceholder.typicode.com/users')
-			.then(res => {
-				const persons = res.data;
-				this.setState({ persons });
-			})
+		.then(res => {
+			const persons = res.data;
+			this.setState({ persons });
+		})
 	}
 
 	render () {
