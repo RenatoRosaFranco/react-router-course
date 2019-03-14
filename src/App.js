18048f233d338609b0fa0680d2697ad9708929b6
Renato Rosa Franco <Renato_ny@live.com>
2019-03-14 13:22:52 -0300

    Project updated

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           <Switch>
             <Route path='/' component={Home} exact/>
             <Route path='/about' component={About}/>
-            <Route path='/project/1' component={Project}/>
+            <Route path='/project/:id' component={Project}/>
             <Route path='/contact' component={Contact}/>
             <Route component={Error} />
           </Switch>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Project extends Component {
+
 	render(){
 		return(
 			<div class='jumbotron'>
