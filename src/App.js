14687e54a36b5df23c230a03446b7dd2b6aace3a
Renato Rosa Franco <Renato_ny@live.com>
2019-03-14 12:23:39 -0300

    React Router - Course

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home       from  './components/Home';
+import About      from  './components/About';
+import Contact    from  './components/Contact';
+import Error      from  './components/Error';
+import Navigation from  './components/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} exact/>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={Contact}/>
+          <Route component={Error} />
+        </Switch>
+      </BrowserRouter> 
+    );
+  }
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+     <span>About page.</span>
+    );
+  }
+}
+
+export default About;
